f4d54503d2e7aab6f8ecfbcd99c406c9ead3a2ba
AlexCauich1607 <124816788+AlexCauich1607@users.noreply.github.com>
2025-10-02 17:55:21 -0600

    Update QueriesBase.java

--- QueriesBase.java
+++ QueriesBase.java
@@ -1,9 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package models;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -281,3 +275,4 @@ public class QueriesBase extends ConnectionDB {
     }
 
 }
+
