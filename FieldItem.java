55ee80c8263977a602eed128ead3c8f139304f06
AlexCauich1607 <124816788+AlexCauich1607@users.noreply.github.com>
2025-10-02 18:13:45 -0600

    Upload FieldItem

---
+++ FieldItem.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models;
+
+/**
+ *
+ * @author alexc
+ */
+public class FieldItem {
+
+    private String field;
+    private String type;
+    private Object value;
+
+    public FieldItem(String field, String type, Object value) {
+        this.field = field;
+        this.type = type;
+        this.value = value;
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
+    }
+}
